e38bdab933178269ae5c96be6d245eadce5b0f1a
Bonobotokin <vangaytech.320@yahoo.com>
2023-09-15 07:09:55 +0300

    Envoye email

--- app/Http/Controllers/PdfExtractionController.php
+++ app/Http/Controllers/PdfExtractionController.php
@@ -2,27 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PdfExtractionRequest;
+use App\Mail\PdfExtractionMail;
+use App\Models\PdfExtraction;
 use Spatie\PdfToText\Pdf;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class PdfExtractionController extends Controller
 {
+
     public function extract(Request $request)
     {
-        $request->validate([
+        $dataSend = $request->validate([
             'pdfFile' => 'required|mimes:pdf|max:2048', // PDF validation rules
         ]);
 
+
+
         try {
             $pdfFilePath = $request->file('pdfFile')->getPathname();
+            $pdfFileName = $request->file('pdfFile')->getClientOriginalName();
 
-            $pdf = Pdf::getText($pdfFilePath);
-            dd($pdf);
-            // $text = $pdf->text();
+            // Use the Pdf::getText() method to directly extract text from the PDF
+            $text = Pdf::getText($pdfFilePath);
 
-            return back()->with('text', $text);
+            // Create a new PdfExtraction instance
+            $pdfExtraction = PdfExtraction::create([
+                'user_id' => auth()->user()->id,
+                'titre' => $pdfFileName,
+                'text' => $text,
+            ]);
+
+            Mail::send(new PdfExtractionMail(['titre' => $pdfExtraction['titre']]));
+
+            return back()->with('text', $text)->with('pdfFileName', $pdfFileName);
         } catch (\Exception $e) {
-            // Handle any exceptions that might occur during PDF processing
             return back()->with('error', 'An error occurred during PDF processing.');
         }
     }
---
+++ resources/views/emails/pdf/extraction.blade.php
@@ -0,0 +1,10 @@
+<x-mail::message>
+    # Nouveaux extraction pdf
+
+    L'extraction de votre pdf ( {{$data['titre']}} ) a été effectuée avec succès.
+
+
+
+    Thanks,
+    {{ config('app.name') }}
+</x-mail::message>
